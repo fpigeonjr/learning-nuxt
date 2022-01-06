874190f12c392c99a8c1f3d85a7c3c64071d5905
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-05 18:01:34 -0600

    adds main sections

---
+++ nuxt-blog/pages/posts/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="single-post update-form">
+    <section class="post">
+      <h1>Title of Post</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>
+        <div>written by NAME</div>
+      </div>
+      <p>Content of post.</p>
+    </section>
+    <section class="post-feedback">
+      <p>
+        Let me know whast you think about the post, please send an email
+        <a href="mailto:frank.pigeonjr@gmail.com">frank.pigeonjr@gmail.com</a>
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
---
+++ nuxt-blog/pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="posts-page">
+    <h1>All the posts</h1>
+  </div>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
