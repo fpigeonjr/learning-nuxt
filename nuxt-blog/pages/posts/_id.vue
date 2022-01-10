4d99c18d75019ce810e3674f803df68289828a34
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-09 19:46:15 -0600

    gets data for single post

--- nuxt-blog/pages/posts/_id.vue
+++ nuxt-blog/pages/posts/_id.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="single-post update-form">
     <section class="post">
-      <h1>Title of Post</h1>
+      <h1>{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div>Last updated on XXX</div>
-        <div>written by NAME</div>
+        <div>Last updated on {{ loadedPost.updatedDate }}</div>
+        <div>written by {{ loadedPost.author }}</div>
       </div>
-      <p>Content of post.</p>
+      <p>{{ loadedPost.content }}</p>
     </section>
     <section class="post-feedback">
       <p>
@@ -16,6 +16,27 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: "1",
+          title: "First Post (ID: " + context.route.params.id + ")",
+          previewText: "Hello World.",
+          author: "Frank Pigeon",
+          updatedDate: new Date(),
+          content: "Some dummy text that is way better than the preview text.",
+          thumbnail:
+            "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
+        },
+      });
+    }, 1000);
+  },
+};
+</script>
 
 <style scoped>
 .update-form {
