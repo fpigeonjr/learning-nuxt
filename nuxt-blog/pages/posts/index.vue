ec6f2761651b31e977891705c43dc863f5b8b2da
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-09 19:55:00 -0600

    refactors to use Promise

--- nuxt-blog/pages/posts/index.vue
+++ nuxt-blog/pages/posts/index.vue
@@ -9,27 +9,35 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "Hello World.",
-            thumbnail:
-              "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post. ",
-            thumbnail:
-              "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-          },
-        ],
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "Hello World.",
+              thumbnail:
+                "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post. ",
+              thumbnail:
+                "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
+            },
+          ],
+        });
+      }, 1500);
+    })
+      .then((data) => {
+        return data;
+      })
+      .catch((e) => {
+        context.error(new Error());
       });
-    }, 1500);
   },
   name: "PostsPage",
 };
