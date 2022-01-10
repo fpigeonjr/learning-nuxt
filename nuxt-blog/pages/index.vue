41fc178f826911629522bc3bdd51050e8aed0fe3
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-09 20:48:14 -0600

    uses NuxtServerInit to initialize the store

--- nuxt-blog/pages/index.vue
+++ nuxt-blog/pages/index.vue
@@ -13,52 +13,10 @@ export default {
   components: {
     PostList,
   },
-  // data() {
-  //   return {
-  //     loadedPosts: [],
-  //   };
-  // },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "Hello World.",
-            thumbnail:
-              "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post. ",
-            thumbnail:
-              "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-          },
-        ],
-      });
-    }, 1500);
-  },
-  created() {
-    setTimeout(() => {
-      this.loadedPosts = [
-        {
-          id: "1",
-          title: "First Post",
-          previewText: "Hello World.",
-          thumbnail:
-            "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-        },
-        {
-          id: "2",
-          title: "Second Post",
-          previewText: "This is our second post. ",
-          thumbnail:
-            "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
-        },
-      ];
-    }, 1500);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
   name: "IndexPage",
 };
--- nuxt-blog/store/index.js
+++ nuxt-blog/store/index.js
@@ -11,6 +11,29 @@ const createStore = () => {
       },
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit("setPosts", [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "Hello World.",
+                thumbnail:
+                  "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is our second post. ",
+                thumbnail:
+                  "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2840&q=80",
+              },
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
       },
